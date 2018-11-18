9ed3f5b2b42aacf6257deedc56c8a97f8ad289e5
Pumpa <stephane.berthel@gmail.com>
2018-11-18 12:59:28 -0500

    Adding simple stories list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Layout from './component/UI/Layout';
+import Stories from './component/Stories/Stories';
 
 class App extends Component {
   render() {
-    return <Layout title="Hackernews top 10 stories">Hello World</Layout>;
+    return (
+      <Layout title="Hackernews top 10 stories">
+        <Stories />
+      </Layout>
+    );
   }
 }
 
